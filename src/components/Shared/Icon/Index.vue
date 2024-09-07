1f11f56c33fdf8e9e6b5ca2cec0e4f123a99d558
aili <ahmadsuhaili244@gmail.com>
2024-09-07 08:33:19 +0700

    tawk-to: refactor icon component

--- src/components/Shared/Icon/Index.vue
+++ src/components/Shared/Icon/Index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="icon-container">
-    <img :src="iconImage" :alt="icon" />
+    <img :src="iconImage" :alt="altText" />
   </div>
 </template>
 
 <script>
 export default {
-  name:'IconComponent',
+  name: 'IconComponent',
   props: {
     icon: {
       type: String,
@@ -14,21 +14,24 @@ export default {
     },
   },
   data() {
-    const listPath = [
-      { id: "play", path: "/assets/images/play.svg" },
-      { id: "comment", path: "/assets/images/message-circle.png" },
-      { id: "desktop", path: "/assets/images/monitor.svg" },
-      { id: "link", path: "/assets/images/link.svg" },
-      { id: "gem", path: "/assets/images/advanced_features.svg" },
-      { id: "shopping-cart", path: "/assets/images/shopping-cart.svg" },
-    ];
     return {
-      listPath,
+      listPath: [
+        { id: "play", path: "/assets/images/play.svg" },
+        { id: "comment", path: "/assets/images/message-circle.png" },
+        { id: "desktop", path: "/assets/images/monitor.svg" },
+        { id: "link", path: "/assets/images/link.svg" },
+        { id: "gem", path: "/assets/images/advanced_features.svg" },
+        { id: "shopping-cart", path: "/assets/images/shopping-cart.svg" },
+      ],
     };
   },
   computed: {
     iconImage() {
-      return this.listPath.find((x) => x.id === this.icon).path;
+      const icon = this.listPath.find((x) => x.id === this.icon);
+      return icon ? icon.path : '/assets/images/default-icon.svg'; 
+    },
+    altText() {
+      return `Icon for ${this.icon}`; 
     },
   },
 };
@@ -38,6 +41,9 @@ export default {
 .icon-container {
   text-align: center;
   padding: 20px;
-}
 
+  img {
+    height: auto;
+  }
+}
 </style>
